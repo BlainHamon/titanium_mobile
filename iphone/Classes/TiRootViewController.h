81ce282fd3027767a2098488b126ec7fd6cb2c17
Blain Hamon <bhamon@bhamon-mbp2.local>
2011-10-05 10:15:30 -0700

    Realigning TiRootViewController more in line with iOS view controllers, esp for iOS 5.

--- iphone/Classes/TiRootViewController.h
+++ iphone/Classes/TiRootViewController.h
@@ -50,21 +50,56 @@
 
 /*
  *	Public APIs are going to get an overhaul to more closely resemble an iOS
- *	
+ *	setup. However, iOS presents a variety of viewController presentation
+ *	methods, especially since iOS 5. When looking at things like
+ *	navigationController, there's even more. So we should have something
+ *	similar that wrappers the various methods. Since we have the 'window'
+ *	metaphor of opening, let's use that.
+ *
+ *	However, for precidence reasons, and because non-TiWindowControllers
+ *	do not have a modal flag, we should follow the precident of tab and
+ *	navigation controllers of having one pair of methods for nonmodal
+ *	presentations, and another for modal presentations.
  */
 
+-(void)openViewController: (UIViewController *)viewController animated:(BOOL)animated;
+-(void)closeViewController: (UIViewController *)viewController animated:(BOOL)animated;
 
-#pragma mark Unsorted API:
+/*
+ *	This would have been present modal/dismiss modal, but iOS 5 changed the
+ *	naming. To be forward-looking, we're going to follow iOS5's pattern.
+ */
+
+- (void)presentViewController:(UIViewController *)viewControllerToPresent animated: (BOOL)flag completion:(void (^)(void))completion;
+- (void)dismissViewControllerAnimated: (BOOL)flag completion: (void (^)(void))completion;
+
+/*
+ *	iOS 5 FINALLY allows us to trigger another round of checking for desired
+ *	orientations. However, since we still need to support iOS 4.x, we still
+ *	need to manually override the situation. So in iOS 5, this merely calls
+ *	super, but iOS 4 has our magic blend. 
+ */
++ (void)attemptRotationToDeviceOrientation;
+
+/*
+ *	A means to tweak the view of the root view controller. With iOS 5, this
+ *	should be done in a way that supports the 
+ */
+@property(nonatomic,readwrite,retain)	UIColor * backgroundColor;
+@property(nonatomic,readwrite,retain)	UIImage * backgroundImage;
 
 
 
+#pragma mark Private API:
+
+-(void)manuallyRotateToOrientation:(UIInterfaceOrientation)newOrientation duration:(NSTimeInterval)duration;
 
 @property(nonatomic,readonly) BOOL keyboardVisible;
 @property(nonatomic,readonly) UIImageView * defaultImageView;
 -(void)dismissDefaultImageView;
 
-@property(nonatomic,readwrite,retain)	UIColor * backgroundColor;
-@property(nonatomic,readwrite,retain)	UIImage * backgroundImage;
+
+#pragma To remove and replace
 
 -(UIViewController *)focusedViewController;
 
@@ -75,7 +110,6 @@
 -(void)repositionSubviews;
 
 -(void)manuallyRotateToOrientation:(UIInterfaceOrientation)orientation;
--(void)manuallyRotateToOrientation:(UIInterfaceOrientation)newOrientation duration:(NSTimeInterval)duration;
 
 -(void)setOrientationModes:(NSArray *)newOrientationModes;
 
